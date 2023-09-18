20fa6f18ae19b6f1f262f3fb1094e71a11ab6416
Rutujajadhav03 <95375630+Rutujajadhav03@users.noreply.github.com>
2023-09-18 20:02:41 +0530

    Add files via upload

---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+export default function Navbar(){
+    return (
+        <nav className ="nav">
+        <a href ="/" className="Atlas Copco">
+            Atlas Copco
+        </a>
+        <ul>
+            <li>
+                <a href ="/Projects">Projects</a>
+            </li>
+            <li>
+                <a href ="/My Profile">My Profile</a>
+            </li>
+        </ul>
+    </nav>
+    )
+}
